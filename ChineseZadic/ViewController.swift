09f1f1713b4ebc29b980f1e69cf8413ddb32c785
maczyt <369280416@qq.com>
2017-08-19 15:53:59 +0800

    init repo

--- ChineseZadic/ViewController.swift
+++ ChineseZadic/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var yearOfBirth: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        yearOfBirth.resignFirstResponder()
+    }
+    
+    @IBAction func okTapped(_ sender: UIButton) {
+        yearOfBirth.resignFirstResponder()
+        if let year = Int(yearOfBirth.text!) {
+           let imageNumber = (year - 4) % 12
+           image.image = UIImage(named: "\(imageNumber)")
+        } else {
+            // notify user
+        }
+    }
 }
 
