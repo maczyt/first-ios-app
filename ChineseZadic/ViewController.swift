7e879ff67593479fb5b0d29b3110c5ea959020fa
maczyt <369280416@qq.com>
2017-08-19 11:00:27 +0800

    Initial Commit

---
+++ ChineseZadic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChineseZadic
+//
+//  Created by 周宇涛 on 2017/8/19.
+//  Copyright © 2017年 maczyt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
